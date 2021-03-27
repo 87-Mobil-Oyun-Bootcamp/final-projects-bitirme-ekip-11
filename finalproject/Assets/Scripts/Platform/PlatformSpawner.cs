5baf861486711f796c117c7402d462990a82a629
gulnihal <k.glnhl@gmail.com>
2021-03-28 00:00:18 +0100

    platform spawn - sevval

---
+++ finalproject/Assets/Scripts/Platform/PlatformSpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class PlatformSpawner : MonoBehaviour
+{
+    public List<GameObject> platforms;
+    private float offset = 25f;
+    void Start()
+    {
+        if(platforms != null && platforms.Count>0)
+        {
+            platforms = platforms.OrderBy(r => r.transform.position.z).ToList();
+        }
+    }
+
+    // Update is called once per frame
+    public void MovePlatform()
+    {
+        GameObject movedPlatform = platforms[0];
+        platforms.Remove(movedPlatform);
+        float newZ = platforms[platforms.Count - 1].transform.position.z + offset;
+        movedPlatform.transform.position = new Vector3(0, 0, newZ);
+        platforms.Add(movedPlatform);
+    }
+}
