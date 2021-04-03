30c60c2019f290e69c5467897e885f55ab5bdbbb
sevvaluyanik <sevvaluyaniik@gmail.com>
2021-04-03 20:11:18 +0300

    Bullet

---
+++ finalproject/Assets/Scripts/Player/Move.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    private float startX;
+    private float lastX;
+    private float startY;
+    private float lastY;
+    private float distanceX;
+    private float distanceY;
+    private float moveValueX;
+    private float moveValueY;
+    public SpawnManager spawnManager;
+
+    void Update()
+    {
+        transform.Translate(transform.forward * 10 * Time.deltaTime, Space.World);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            startX = Input.mousePosition.x;
+            startY = Input.mousePosition.y;
+
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            lastX = Input.mousePosition.x;
+            distanceX = lastX - startX;
+            moveValueX = (distanceX / Screen.width) * 50;
+
+            lastY = Input.mousePosition.y;
+            distanceY = lastY - startY;
+            moveValueY = (distanceY / Screen.height) * 50;
+
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x + moveValueX, -5, 5), Mathf.Clamp(transform.position.y + moveValueY, -5, 6), transform.position.z);
+
+            startX = lastX;
+            startY = lastY;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.transform.root.gameObject.CompareTag("Platform"))
+        {
+            spawnManager.SpawnTriggerEntered();
+            Debug.Log("platform spawned!");
+        }
+
+
+    }
+}
